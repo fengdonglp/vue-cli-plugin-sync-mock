16c9f4f5fa4a1af51f64235b7fa44ea493a980d6
fd <fengdonglp@163.com>
2019-12-27 16:26:49 +0800

    feat: Generate the first available version
    BREAKING CHANGE:
    1.支持sync插件选项是否同步服务端数据；
    2.支持mock插件选项是否开启本地mock服务（代理返回已同步的数据）；
    3.支持cachePath插件选项将同步数据缓存在任意目录；

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-module.exports = (api, projectConfig) => {};
+const path = require('path')
+const bodyParser = require('body-parser')
+const { mixProxy } = require('./lib/proxy')
+const setup = require('./lib/mock')
+
+const PLUGIN_OPTION_NAME = 'syncMock'
+const ROOT_PATH = path.resolve(__dirname).split('/node_modules')[0]
+const DEFAULT_CACHE_PATH = path.resolve(ROOT_PATH, './node_modules/.cache/vue-cli-plugin-sync-mock-data/')
+
+module.exports = (api, projectConfig) => {
+  /**
+   * 不能使用 api.chainWebpack 和 api.configureWebpack 修改 devServer 选项，会被用户配置覆盖
+   * @vue/cli-service/lib/commands/serve.js line82-85 使用Object.assign直接用用户配置覆盖devServer，导致配置不生效
+   */
+
+  if (!projectConfig.pluginOptions || !projectConfig.pluginOptions[PLUGIN_OPTION_NAME]) {
+    return
+  }
+
+  const {
+    devServer: { before: _before, proxy: _proxy },
+    pluginOptions: { [PLUGIN_OPTION_NAME]: config }
+  } = projectConfig
+
+  const syncConfig = config || {}
+  // 是否开启mock，从本地存储的数据中获取
+  const isMock = !!syncConfig.mock
+  // 是否开启同步数据
+  const isSync = !!syncConfig.sync
+  const cachePath = config.cachePath || DEFAULT_CACHE_PATH
+
+  projectConfig.devServer.before = (app, server) => {
+    if (isMock) {
+      setup(app, cachePath)
+    }
+
+    _before(app, server)
+
+    if (isSync) {
+      app.use(bodyParser.urlencoded({ extended: false }))
+      app.use(bodyParser.json())
+    }
+  }
+
+  projectConfig.devServer.proxy = mixProxy(_proxy, isSync, cachePath)
+}
